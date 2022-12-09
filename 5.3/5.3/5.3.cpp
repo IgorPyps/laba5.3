2fc77acc0444823797db6d19a9e63680b0d57164
IgorPyps <115737998+IgorPyps@users.noreply.github.com>
2022-12-09 02:26:56 +0200

    Add files via upload

---
+++ 5.3/5.3/5.3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+double h(const double x);
+
+int main()
+{
+	double qp, qk, z;
+	int n;
+	cout << "qp = "; cin >> qp;
+	cout << "qk = "; cin >> qk;
+	cout << "n = "; cin >> n;
+	double dg = (qk - qp) / n;
+	cout << dg << endl;
+	double q = qp;
+	while (q <= qk)
+	{
+		z = h(q + 1) + h((q * q) + 1) + pow(h(q * q), 2);
+		cout << q << " " << z << endl;
+		q += dg;
+	}
+	return 0;
+}
+
+double h(const double x)
+
+{
+	if (abs(x) >= 1)
+		return ((cos(x) + 1) / ((cos(x) * cos(x)) + 1));
+	else
+	{
+		double S = 0;
+		int i = 0, fac = 1, n = 1;
+		double a = 1 / cos(x);
+		S = 0;
+
+		do
+		{
+			for (n = 1; n <= (2 * i); n++) {
+				fac *= n;
+			}
+			i++;
+			S += (pow(x, 2 * i) / fac);
+		} while (i < 6);
+		return S;
+
+
+	}
+}
---
+++ 5.3/unit 5.3/unit 5.3.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.3/5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit53
+{
+	TEST_CLASS(unit53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreNotEqual(h(1), 3.);
+		}
+	};
+}
